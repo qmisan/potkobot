e1705c0119a200c6902a46806ac234ac481468e2
Henri Merilä <henri.merila@fi.abb.com>
2018-01-18 18:07:34 +0200

    iota / btc commands

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json
+import requests
 from telegram.ext import Updater, CommandHandler
 
+# Globals
 token = "498370125:AAHnCOBNJldxxvn2vHKNJAjjy0u7-nDLwWU"
+coin_market_cap_api_url = "https://api.coinmarketcap.com/v1/ticker/"
 
 
 def hello(bot, update):
@@ -8,9 +12,32 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name))
 
 
+def iota(bot, update):
+    suffix = "iota/"
+    response = requests.get(coin_market_cap_api_url + suffix)
+    json_data = json.loads(response.text)
+    update.message.reply_text("Ei hätää pojat raketoi ihan just ;)\n" +
+                              "IOTA = {0} USD".format(json_data[0]["price_usd"]))
+
+
+def bitcoin(bot, update):
+    suffix = "bitcoin/"
+    response = requests.get(coin_market_cap_api_url + suffix)
+    json_data = json.loads(response.text)
+    update.message.reply_text("Ai että bitcoin fanboy ;)\n" +
+                              "BTC = {0} USD".format(json_data[0]["price_usd"]))
+
+
+def lauri(bot, update):
+    update.message.reply_text("Suomen humaanein viestijä!")
+
+
 updater = Updater(token)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('iota', iota))
+updater.dispatcher.add_handler(CommandHandler('bitcoin', bitcoin))
+updater.dispatcher.add_handler(CommandHandler('lauri', lauri))
 
-updater.start_polling()
-updater.idle()
+updater.start_polling(poll_interval=1)
+# updater.idle()
